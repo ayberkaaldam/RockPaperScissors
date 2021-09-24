be69890353d39633c1962fcaea6f0c9475102e30
Ayberk ALDAM <72295798+Bebuay@users.noreply.github.com>
2021-09-24 23:57:21 +0300

    how to play page

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const dropdownButton = document.getElementById("dropdown-btn");
 dropdownButton.addEventListener("click",()=>{
     let dropdownContent = document.getElementById("dropdown-container");
+    let completeCircle = document.getElementById("complete-circle");
     if (dropdownContent.style.display == "flex") {
       dropdownContent.style.display = "none";
+      completeCircle.style.display="none";
     } else {
       dropdownContent.style.display = "flex";
+      completeCircle.style.display="block";
     }
 })
